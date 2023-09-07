7e7b698119b39a1a79206f56199ed15f65666ae6
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-09-07 21:47:33 +0000

    code ready just need to be polished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React,{ useState } from "react";
+import "./home.css";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const[glowLight,setGlowLight]=useState("red")
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Traffic Light </h1>
+			<div className="wrapper">
+				<div className="trafficLHead bg-dark ">
+					<button type="button" className={"red"+(glowLight==="red"?" glow":"")} onClick={() => {setGlowLight("red")}}
+					><b className="text-center text-light">Stop</b>
+					</button>	
+					<button type="button" className={"orange"+(glowLight==="orange"?" glow":"")} onClick={() => {setGlowLight("orange")}}
+					><b className="text-center text-light">Warning</b></button>
+					<button type="button" className={"green"+(glowLight==="green"?" glow":"")} onClick={() => {setGlowLight("green")}}>
+					<b className="text-center text-light">Go</b>	</button>
+				</div>
+				<div className='foot'></div>
+			</div>
+		
+			
 		</div>
 	);
 };
